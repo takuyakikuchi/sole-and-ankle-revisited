ce63c813114a6b04f4b51b6b6e77689b041af976
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-02-23 16:22:18 +0900

    Excercise 3: tablet

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, QUERIES } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -47,10 +47,26 @@ const Wrapper = styled.div`
   flex-direction: row-reverse;
   align-items: baseline;
   gap: 32px;
+
+  @media ${QUERIES.tabletAndDown} {
+    flex-direction: column-reverse;
+    gap: 0;
+  }
 `;
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media ${QUERIES.tabletAndDown} {
+    flex-basis: 0;
+
+    /* Hiding all but Breadcrumbs */
+    > * {
+      &:not(:first-child) {
+        display: none;
+      }
+    }
+  }
 `;
 
 const MainColumn = styled.div`
