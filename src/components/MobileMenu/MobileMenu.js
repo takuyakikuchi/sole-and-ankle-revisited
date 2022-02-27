3684be42f598e49a0cc98dcc7f20e48db2fb1a00
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-02-27 14:11:58 +0900

    Exercise 4: Mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { QUERIES } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
@@ -15,23 +15,87 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <StyledDialogOverlay isOpen={isOpen} onDismiss={onDismiss}>
+      <StyledDialogContent>
+        <CloseButton onClick={onDismiss}>
+          <Icon id="close" strokeWidth={1} />
+        </CloseButton>
+        
+        <MenuNav>
+          <NavLink active href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </MenuNav>
+
+        <MenuFooter>
+          <FooterLink href="/terms">Terms and Conditions</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/contact">Contact Us</FooterLink>
+        </MenuFooter>
+      </StyledDialogContent>
+    </StyledDialogOverlay>
   );
 };
+
+export const StyledDialogOverlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: hsl(220deg 5% 40% / 0.8);
+`;
+
+export const StyledDialogContent = styled(DialogContent)`
+  --spacing: 32px;
+
+  position: absolute;
+  background: ${COLORS.white};
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 20rem;
+  padding: var(--spacing);
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CloseButton = styled(UnstyledButton)`
+  position: absolute;
+  top: var(--spacing);
+  right: 22px;
+`;
+
+export const NavLink = styled.a`
+  text-decoration: none;
+  color: ${props => props.active? COLORS.secondary : COLORS.gray[900]};
+  font-weight: ${WEIGHTS.medium};
+  font-size: calc(18 / 16 * 1rem);
+`;
+
+export const MenuNav = styled.nav`
+  margin: auto 0;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+
+export const FooterLink = styled.a`
+  text-decoration: none;
+  color: ${COLORS.gray[700]};
+  font-size: calc(14 / 16 * 1rem);
+`;
+
+export const MenuFooter = styled.footer`
+  position: absolute;
+  bottom: var(--spacing);
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
 
 export default MobileMenu;
